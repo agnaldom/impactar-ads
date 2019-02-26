da9f9c23dadef8c6a9755d00d4f50a3ed8a90122
Agnaldo marinho <agnaldomarinho7@gmail.com>
2019-02-25 21:33:26 -0300

    Adicionando o codigo da aula4

---
+++ logica-de-programacao/aula4.py
@@ -0,0 +1,10 @@
+# Estrutura de selecao encadeada
+#
+# O programa verifica se o numero é divisivel por 3 ou por 5
+numero = int(input("Digite um número: "))
+if numero % 3 == 0:
+    print("Divisivel por 3")
+elif numero % 5 == 0:
+    print("Divisivel por 5")
+else:
+    print("Numeros nao divisiceis")
