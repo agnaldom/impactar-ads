f70b467a2ed6778e914c65d94c564554a3c1e25f
Agnaldo marinho <agnaldomarinho7@gmail.com>
2019-02-25 21:34:00 -0300

    Adicionando o codigo da aula5

---
+++ logica-de-programacao/aula5-1.py
@@ -0,0 +1,9 @@
+# Variavel acumulador
+# 
+cont = 1
+soma = 0
+numero = int(input("Digite um numero: "))
+while cont <= 10:
+    soma = soma + cont * 2
+    cont = cont + 1
+print(soma)
---
+++ logica-de-programacao/aula5.py
@@ -0,0 +1,7 @@
+# Estrutura de repeticao - While
+#
+cont=1
+numero = int(input("Digite um nuemro"))
+while cont <= 10:
+    print(numero, " x ", cont, " = ", numero * cont)
+    cont = cont + 1
