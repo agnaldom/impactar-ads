b7db345a295d6daca40848b94d6f2ddce47234a0
Agnaldo marinho <agnaldomarinho7@gmail.com>
2019-02-25 21:06:45 -0300

    Codigo da aula3

---
+++ logica-de-programacao/aula3.py
@@ -0,0 +1,8 @@
+# Operadores, expressoes logicas e estrutura de selecao
+#
+# Verificar se o numero digita é impor ou par
+numero = int(input("Digite um numero: "))
+if numero % 2 == 0:
+    print("Número para")
+else:
+    print("Número impar")
