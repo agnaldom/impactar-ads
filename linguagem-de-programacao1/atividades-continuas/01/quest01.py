f109ca9f1e371354d2222dc6e05185d19874be99
Agnaldo marinho <agnaldomarinho7@gmail.com>
2019-03-10 15:08:35 -0300

    Atividade conitnuas - 01 - qt01

---
+++ linguagem-de-programacao1/atividades-continuas/01/quest01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Faca um programa que solicite ao usuarios as 4 notas de atividades
+# continuas e mostre a media. Lembrando que, o total de atividades 
+# continuas consideradas para o calculo da media sera um total de sete.
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+nota3 = int(input("Digite a terceira nota: "))
+nota4 = int(input("Digite a quarta nota: "))
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print("A media e: ", media)
