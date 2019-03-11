2f50ccedc2f8a8d61e0877dd1226cea68d426069
Agnaldo marinho <agnaldomarinho7@gmail.com>
2019-03-11 19:47:32 -0300

    Adicionando atividade 02

---
+++ linguagem-de-programacao1/atividades-continuas/02/quest01.py
@@ -0,0 +1,8 @@
+# Escreva linhas de comando para:
+# a) Receber um dado, exibindo a mensagem: Qual a quantidade de alunos?
+# b) Exibir o conteúdo da variável.
+# c) Exibir o tipo da variável
+qtAlunos = int(input("Entre com a quatidade de alunos: "))
+print(qtAlunos)
+
+print(type(qtAlunos))
